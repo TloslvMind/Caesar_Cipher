917822e75296cfbeaa0009be2a17bee198931539
Tysiachnyk <tloslvmind@gmail.com>
2024-10-28 20:37:43 +0200

    Small fix to increase productivity

--- functions.py
+++ functions.py
@@ -2,10 +2,11 @@ from string import ascii_letters
 
 def caesar(original_message, shift_number, encode_or_decode):
     result = ""
-    k = 1 if encode_or_decode == "encode" else -1
+    if encode_or_decode == "decode":
+        shift_number *= -1
     for letter in original_message:
         if letter in ascii_letters:
-            result += ascii_letters[(ascii_letters.index(letter) + shift_number * k) % 26]
+            result += ascii_letters[(ascii_letters.index(letter) + shift_number) % 26]
         else:
             result += letter
     return f"Here's the {encode_or_decode}d result: {result}"
