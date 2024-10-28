5c4b0cb41fe1d7709204bac178ef495858dd2b2f
Tysiachnyk <tloslvmind@gmail.com>
2024-10-28 19:04:10 +0200

    Added two modules (functions, logo). Replaced functions from main into 'functions' and added introduсtory image into logo module

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-from string import ascii_letters
+from functions import encrypt, decrypt
+from logo import logo
 
-
-def encrypt(original_message, shift_number):
-    result = ""
-    for letter in original_message:
-        if letter in ascii_letters:
-            result += ascii_letters[(ascii_letters.index(letter) + shift_number) % 26]
-        else:
-            result += letter
-    return f"Here's the encoded result: {result}"
-
-
-def decrypt(original_message, shift_number):
-    result = ""
-    for letter in original_message:
-        if letter in ascii_letters:
-            result += ascii_letters[(ascii_letters.index(letter) - shift_number) % 26]
-        else:
-            result += letter
-    return f"Here's the decoded result: {result}"
-
+print(logo)
 
 while True:
     choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
